821d246d8e5f910276a762cc6ca6f73cda2ca2c0
yerkebulan19971212 <yerkebulan19971212@gmail.com>
2020-03-22 17:08:56 +0600

    add manager.py

--- smart_note_diploma/users/models.py
+++ smart_note_diploma/users/models.py
@@ -1,47 +1,15 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import (
-    AbstractBaseUser, PermissionsMixin, BaseUserManager
-)
+from django.contrib.auth.models import ( AbstractBaseUser, PermissionsMixin)
 
-
-class UserManager(BaseUserManager):
-
-    use_in_migrations = True
-
-    def _create_user(self, email, password, **extra_fields):
-        """
-        Creates and saves a User with the given email,and password.
-        """
-        if not email:
-            raise ValueError('The given email must be set')
-        try:
-            # with transaction.atomic():
-            user = self.model(email=email, **extra_fields)
-            user.set_password(password)
-            user.save(using=self._db)
-            return user
-        except:
-            raise
-
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self._create_user(email, password=password, **extra_fields)
+from smart_note_diploma.users.manager import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     """
        An abstract base class implementing a fully featured User model with
        admin-compliant permissions.
-
     """
     email = models.EmailField(max_length=40, unique=True)
     first_name = models.CharField(max_length=30, blank=True)
