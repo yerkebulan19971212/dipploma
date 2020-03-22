38beee1fd069b38553696adf28500c808ae2b8bf
yerkebulan19971212 <yerkebulan19971212@gmail.com>
2020-03-22 17:08:21 +0600

    add model 'notebooks'

--- smart_note_diploma/notes/models.py
+++ smart_note_diploma/notes/models.py
@@ -4,4 +4,9 @@ from smart_note_diploma.core.models import (TimeStampedModel, Hashtags, )
 
 class Note(TimeStampedModel):
     name = models.CharField(max_length=250)
-    hash_tag = models.ManyToManyField(Hashtags, blank=True)
+    hash_tags = models.ManyToManyField(Hashtags, blank=True)
+
+
+class NoteBooks(TimeStampedModel):
+    name = models.CharField(max_length=250)
+    notes = models.ManyToManyField(Note, blank=True)
