c62d32b1f01fb896e7c8790dae74620ea235869c
yerkebulan19971212 <yerkebulan19971212@gmail.com>
2020-04-16 13:51:29 +0600

    authenticate

--- smart_note_diploma/users/urls.py
+++ smart_note_diploma/users/urls.py
@@ -2,9 +2,10 @@
 from django.urls import path
 
 # local imports
-from smart_note_diploma.users.api.views import CreateUser
+from smart_note_diploma.users.api.views import CreateUser, authenticate_user
 
 app_name = "users"
 urlpatterns = [
     path("create/", CreateUser.as_view()),
+    path("login/", authenticate_user)
 ]
